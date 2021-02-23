00d38c7254a674f845695f684c44d47b455ecb43
Chris <cciric44@gmail.com>
2021-02-22 23:35:22 -0500

    got a token to print in console

--- www/js/welcome.js
+++ www/js/welcome.js
@@ -8,18 +8,21 @@ function login(){
     console.log(password.value);
 
     let login = {
-        "userName": username.value,
-        "password": password.value
+        userName: username.value,
+        password: password.value
     };
+
+    console.log(JSON.stringify(login))
+    console.log(login);
 
     fetch('http://localhost:8080/authenticate', {
            method: 'POST',
            headers: {
                
                 'Content-Type': 'application/json',
-                'Access-Control-Allow-Methods': 'POST',
-                'Access-Control-Allow-Headers': 'Content-Type',
-                'Access-Control-Allow-Origin': 'http://localhost:8000'
+                // 'Access-Control-Allow-Methods': 'POST',
+                // 'Access-Control-Allow-Headers': 'Content-Type',
+                // 'Access-Control-Allow-Origin': 'http://localhost:8000'
 
            },
            body: JSON.stringify(login)
@@ -29,6 +32,7 @@ function login(){
         } else {
             throw new Error('Server response wasn\'t OK');
         }
+        return response
     }).then( function(loginToken){
             console.log(loginToken);
         return loginToken;
